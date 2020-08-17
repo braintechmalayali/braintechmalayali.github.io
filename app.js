c5a59cf379fae17d0cd88aec12bef4b68f0f39aa
braintechmalayali <69150391+braintechmalayali@users.noreply.github.com>
2020-08-17 17:36:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const navslide = ()=>{
   const nav = document.querySelector('.nav-links');
 
   burger.addEventListener('click',()=>{
-    nav.classList.toggle('nav-active');
+    nav.classList.toggle('open');
   });
 }
 
